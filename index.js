69a297e13a840e3a354d7f1f3d476f1c862b460b
honeychuckles <kaylaedwards575@gmail.com>
2021-10-06 14:42:26 +0000

    Finished code-challenge

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(abs_num){
+  if(abs_num < 0){
+      return abs_num*-1;
+  }else if (abs_num >=0){
+      return abs_num;
+      
+  }else
+   return null;
 }
 
 
 
 
+//console.log(absoluteValue(10.35));
+//console.log(absoluteValue("hello"));
+//console.log(absoluteValue(-3));
+
+//2 
+function multiplesOffourAndSix (){
+   let array=[];
+    for(let i=1; i<=100; i++){
+        if(i%4 === 0 && i%6 === 0){
+            array.push(i);
+        }
+    }
+    return array
+}
+
+console.log(multiplesOffourAndSix());
